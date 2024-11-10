d7d8e355c751a0e5fc0b44787b6d5224066c1b0e
Macaulay Precious <preciousmacaulay001@gmail.com>
2024-11-10 21:17:17 +0100

    Set base URL to "/imagine/" by default

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,8 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/imagine/",
+  // Set base URL to "/imagine/" by default, or "" if explicitly set to "false"
+  base: process.env.VITE_DEPLOY_TO_SUBDIRECTORY === "false" ? "" : "/imagine/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
